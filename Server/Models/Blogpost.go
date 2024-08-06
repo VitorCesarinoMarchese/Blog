2fe725bf4ba3a37c599ca54b0d8d65514efba9f6
Vitor Cesarino Marchese <vitorcesarino1@gmail.com>
2024-08-06 08:26:16 -0300

    fix

--- Server/Models/Blogpost.go
+++ Server/Models/Blogpost.go
@@ -7,20 +7,30 @@ import (
 
 type BlogPost struct {
 	gorm.Model
-	Title, Img, Description, Author string
-	Views                           int
+	Title       string `json:"Title"`
+	Img         string `json:"Img"`
+	Description string `json:"Description"`
+	Author      string `json:"Author"`
+	Views       int `json:"Views"`
 }
-func CreatePost(post BlogPost){
-	db := db.ConnectDb()
-	db.AutoMigrate(&BlogPost{})
-	result := db.Create(post)
-	if result.Error != nil{
+
+func CreatePost(post *BlogPost) {
+	daba := db.ConnectDb()
+	daba.AutoMigrate(&BlogPost{})
+	result := daba.Create(post)
+	if result.Error != nil {
 		panic(result.Error)
 	}
 }
-func ReadPosts() []BlogPost{
-	db := db.ConnectDb()
+func FindPostById(id uint) BlogPost{
+	var Post BlogPost
+	daba := db.ConnectDb()
+	daba.First(&Post, id)
+	return Post
+}
+func ReadPosts() []BlogPost {
+	daba := db.ConnectDb()
 	var BlogPosts []BlogPost
-	db.Find(&BlogPosts)
+	daba.Find(&BlogPosts)
 	return BlogPosts
 }
--- Server/main.go
+++ Server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strconv"
+
 	m "blog.net/Models"
 	"github.com/gin-gonic/gin"
 )
@@ -14,17 +16,25 @@ func main() {
 			"message": "ok",
 		})
 	})
-	r.POST("/createPost", func(c *gin.Context) {
-		var newPost m.BlogPost
-		if err := c.BindJSON(&newPost); err != nil{
+	r.GET("/post/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		nid, err := strconv.Atoi(id)
+		if err != nil{
 			panic(err)
 		}
+		c.JSON(200, m.FindPostById(uint(nid)))
+	})
+	r.POST("/createPost", func(c *gin.Context) {
+		newPost := &m.BlogPost{}
+		if err := c.BindJSON(&newPost); err != nil{
+			panic("err")
+		}
 		m.CreatePost(newPost)
+		result := m.FindPostById(newPost.ID)
+		c.JSON(200, result)
 	})
 	r.GET("/Posts", func(c *gin.Context){
 		c.JSON(200, m.ReadPosts())
 	})
-
-
 	r.Run(":8000")
 }
